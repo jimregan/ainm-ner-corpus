b73786416ab88ef88860d896e74c3590c07556d7
Jim O'Regan <joregan@gmail.com>
2017-07-12 16:43:11 +0100

    delete

--- src/test/java/ie/tcd/slscs/itut/gramadanj/UtilsTest.java
+++ src/test/java/ie/tcd/slscs/itut/gramadanj/UtilsTest.java
@@ -61,31 +61,6 @@ public class UtilsTest extends TestCase {
         assertEquals(true, Utils.equalLists(a, b));
     }
 
-    public void testEqualsListsWithForm() {
-        List<Form> a = new ArrayList<Form>();
-        List<Form> b = new ArrayList<Form>();
-        Form aa = new Form("ay");
-        Form ab = new Form("bee");
-        Form ac = new Form("cee");
-        Form ba = new Form("ay");
-        Form bb = new Form("bee");
-        Form bc = new Form("cee");
-        a.add(ab);
-        a.add(ac);
-        a.add(aa);
-        b.add(bc);
-        b.add(ba);
-        b.add(bb);
-        assertEquals(true, aa.equals(ba));
-        assertEquals(true, ab.equals(bb));
-        assertEquals(true, ac.equals(bc));
-        assertEquals(true, Utils.equalLists(a, b));
-        b.add(new Form("dee"));
-        assertEquals(false, Utils.equalLists(a, b));
-        a.add(new Form("ee"));
-        assertEquals(false, Utils.equalLists(a, b));
-    }
-
     public void testTrim() {
         assertEquals("", Utils.trim("     "));
         assertEquals("aaa", Utils.trim("  aaa   "));
