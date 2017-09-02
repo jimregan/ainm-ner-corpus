b2eaed47ede37deac0a373fa38e92cbcbceab004
Jim O'Regan <jaoregan@tcd.ie>
2017-09-02 20:39:02 +0100

    yah, broke

--- src/test/java/ie/tcd/slscs/itut/ainmnercorpus/ListPartitionTest.java
+++ src/test/java/ie/tcd/slscs/itut/ainmnercorpus/ListPartitionTest.java
@@ -28,6 +28,21 @@ public class ListPartitionTest {
 
     @Test
     public void makeText() throws Exception {
+        TextEntity txt = new TextEntity("A piece of text. With a sentence. Or three. In ");
+        SimpleEntity se1 = new SimpleEntity("Co. Cork", "place");
+        TextEntity txt2 = new TextEntity(" of all places.");
+        EntityBase ents[] = new EntityBase[]{txt, se1, txt2};
+        Span sents[] = new Span[]{new Span(0, 16), new Span(17, 33), new Span(34, 43), new Span(44, 70)};
+        Span toks[] = new Span[]{
+                new Span(0, 1), new Span(2, 7), new Span(8, 10), new Span(11, 15),
+                new Span(15, 16), new Span(17, 21), new Span(22, 23), new Span(24, 32),
+                new Span(32, 33), new Span(34, 36), new Span(37, 42), new Span(42, 43),
+                new Span(44, 46), new Span(47, 49), new Span(49, 50), new Span(51, 55),
+                new Span(56, 58), new Span(59, 62), new Span(63, 69), new Span(69, 70),
+            };
+        String exp = "A piece of text .\nWith a sentence .\nOr three .\nIn <START:place> Co . Cork <END> of all places .\n";
+        String out = ListPartition.makeText(ents, sents, toks);
+        assertEquals(exp, out);
     }
 
     @Test
